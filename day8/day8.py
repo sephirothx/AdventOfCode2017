95fd7a49f4046a1928df257e4f0f61d566c34576
Stefano Prosperi <step.prosperi@gmail.com>
2020-12-05 21:15:01 +0100

    day 8

---
+++ day8/day8.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+inp = open('input.txt').read().splitlines()
+
+reg = defaultdict(int)
+sol1 = 0
+sol2 = 0
+for l in inp:
+    r1,op1,v1,_,r2,op2,v2 = l.split()
+    v1,v2 = int(v1),int(v2)
+    if op2 == '<':
+        cond = reg[r2] < v2
+    elif op2 == '>':
+        cond = reg[r2] > v2
+    elif op2 == '==':
+        cond = reg[r2] == v2
+    elif op2 == '>=':
+        cond = reg[r2] >= v2
+    elif op2 == '<=':
+        cond = reg[r2] <= v2
+    elif op2 == '!=':
+        cond = reg[r2] != v2
+    if cond:
+        reg[r1] += v1 if op1 == 'inc' else -v1
+        sol2 = max(sol2, reg[r1])
+
+for r in reg:
+    sol1 = max(sol1, reg[r])
+
+print(sol1)
+print(sol2)
