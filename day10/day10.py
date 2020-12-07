f1c7a57f7d693e64b31ec2c48f81b2d6cbdae955
Stefano Prosperi <step.prosperi@gmail.com>
2020-12-07 22:50:15 +0100

    day 10: generalized knot function

--- day10/day10.py
+++ day10/day10.py
@@ -1,28 +1,27 @@
-inp = [ord(c) for c in open('input.txt').read()]
-inp.extend([17, 31, 73, 47, 23])
+inp = open('input.txt').read()
 
 RANGE = 256
 
-pos = 0
-skip = 0
-l = [x for x in range(RANGE)]
+def knot(s):
+    s = [ord(c) for c in s] + [17,31,73,47,23]
+    pos = 0
+    skip = 0
+    l = [x for x in range(RANGE)]
+    for _ in range(64):
+        for i in s:
+            tmp = l[pos:]+l[:pos]
+            tmp[:i] = tmp[:i][::-1]
+            l = tmp[(RANGE-pos):]+tmp[:(RANGE-pos)]
+            pos += (i + skip)
+            pos %= RANGE
+            skip += 1
+    k = 0
+    curr = 0
+    for i in range(RANGE):
+        curr ^= l[i]
+        if (i+1) % 16 == 0:
+            k = (k << 8) + curr
+            curr = 0
+    return k
 
-for _ in range(64):
-    for i in inp:
-        tmp = l[pos:]+l[:pos]
-        tmp[:i] = tmp[:i][::-1]
-        l = tmp[(RANGE-pos):]+tmp[:(RANGE-pos)]
-        pos += (i + skip)
-        pos %= RANGE
-        skip += 1
-
-knot = []
-curr = 0
-for i in range(RANGE):
-    curr ^= l[i]
-    if (i+1) % 16 == 0:
-        knot.append(curr)
-        curr = 0
-
-for i in knot:
-    print(f'{i:0>2x}', end='')
+print(f'{knot(inp):x}')
