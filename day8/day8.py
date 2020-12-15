7c4424fabe7c73e917dd60ccedb401f957d6fb92
Stefano Prosperi <step.prosperi@gmail.com>
2020-12-15 17:01:43 +0100

    day 8: simplified condition switch using dictionary

--- day8/day8.py
+++ day8/day8.py
@@ -2,24 +2,22 @@ from collections import defaultdict
 
 inp = open('input.txt').read().splitlines()
 
+d = {
+    '<': lambda x,y: x<y,
+    '>': lambda x,y: x>y,
+    '==': lambda x,y: x==y,
+    '>=': lambda x,y: x>=y,
+    '<=': lambda x,y: x<=y,
+    '!=': lambda x,y: x!=y,
+}
+
 reg = defaultdict(int)
 sol1 = 0
 sol2 = 0
 for l in inp:
     r1,op1,v1,_,r2,op2,v2 = l.split()
     v1,v2 = int(v1),int(v2)
-    if op2 == '<':
-        cond = reg[r2] < v2
-    elif op2 == '>':
-        cond = reg[r2] > v2
-    elif op2 == '==':
-        cond = reg[r2] == v2
-    elif op2 == '>=':
-        cond = reg[r2] >= v2
-    elif op2 == '<=':
-        cond = reg[r2] <= v2
-    elif op2 == '!=':
-        cond = reg[r2] != v2
+    cond = d[op2](reg[r2], v2)
     if cond:
         reg[r1] += v1 if op1 == 'inc' else -v1
         sol2 = max(sol2, reg[r1])
